36b83240633eff2f219d0972e90655eed088cc6f
milanmitic <miticm97@gmail.com>
2024-12-08 01:20:31 +0100

    Hook v1

--- src/LPYieldSpike.sol
+++ src/LPYieldSpike.sol
@@ -9,23 +9,24 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
-import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
 import {BalanceDeltaLibrary, BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {LiquidityAmounts} from "./libraries/LiquidityAmounts.sol";
 
 import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
 
 import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
 
-contract LPYieldSpike is BaseHook {
+import {VRFV2PlusWrapperConsumerBase} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFV2PlusWrapperConsumerBase.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract LPYieldSpike is BaseHook, VRFV2PlusWrapperConsumerBase {
     // StateLibrary is new here and we haven't seen that before
     // It's used to add helper functions to the PoolManager to read
     // storage values.
@@ -42,13 +43,27 @@ contract LPYieldSpike is BaseHook {
     using LPFeeLibrary for uint24;
     using SafeERC20 for IERC20;
 
-    // Timestamp of the latest prize distribution
-    uint256 public lastPrizeDrawTimestamp;
+    struct LPPositionInfo {
+        int24 tickLower;
+        int24 tickUpper;
+        int256 liquidityDelta;
+        uint256 token0Amount;
+    }
 
     // Draw prize winner every 86400 seconds (24 hours)
     uint256 public constant PRIZE_DRAW_INTERVAL = 86400;
-
+    // Precision used to calculate fee
     uint24 public constant FEE_PRECISION = 10000;
+    // Gas limit for VRF callback
+    uint32 public constant VRF_CALLBACK_GAS_LIMIT = 500_000;
+
+    uint256 public constant VALUE_PER_ENTRY = 10 ** 16;
+
+    // Minimum number of confirmation for Chainlink VRF request
+    uint16 public constant minimumConfirmations = 3;
+
+    // Keep track of link token
+    uint256 public linkTokenBalance;
 
     // Initial fee of the pool
     uint24 public initialPoolFee; // 500 = 5%
@@ -59,17 +74,47 @@ contract LPYieldSpike is BaseHook {
     // Percentage of fee to be allocated for prize
     uint24 public feePercentageForPrize;
 
+    // Track liquidity provider positions for pool
+    mapping(PoolId => mapping(address => LPPositionInfo[] lpPositionInfo)) public lpPositionsInfo;
+
+    mapping(PoolId => address[] lpProvider) public lpProviders;
+
+    mapping(PoolId => uint256 numberOfPosition) public numberOfPositions;
+
+    // Track token0 amount provided by liquidity provider for pool
+    mapping(PoolId => mapping(address => uint256 token0Amount)) public token0Amounts;
+
+    // Track last prize draw timestamp per pool
+    mapping(PoolId poolId => uint256 prizeDrawTimestamp) public lastPrizeDrawTimestamp;
+
+    // Tracks Chinlink VRF fullfilment status per pool.
+    mapping(uint256 chainlinkVRFRequestID => PoolKey poolkey) internal requests;
+
     // Accumulated prize for pool per token
-    mapping(PoolId poolId => mapping(address => uint256)) public accumulatedPrize;
+    mapping(PoolId poolId => mapping(Currency => uint256)) public accumulatedPrize;
+
+
+    mapping(uint256 currentEntryIndex => address lpProvider) public entryIndexToProvider;
+
+    /**
+     * @dev Emitted when $LINK is deposited to the contract.
+     * @param amount is amount of $LINK deposited.
+     */
+    event Deposit(address indexed authorizedFunder, uint256 amount);
 
     error MustUseDynamicFee();
+    error InsufficientBalance();
+    error InvalidChainlinkVRFRequestID();
 
     /// @param _poolManager Address of poolManager
     /// @param _initialPoolFee Initial pool fee
     /// @param _feePercentageForPrize Fee percentage to be taken from fee to accumulate prize (e.g. 500 = 5%)
-    constructor(IPoolManager _poolManager, uint24 _initialPoolFee, uint24 _feePercentageForPrize)
-        BaseHook(_poolManager)
-    {
+    constructor(
+        IPoolManager _poolManager,
+        address _vrfV2PlusWrapper,
+        uint24 _initialPoolFee,
+        uint24 _feePercentageForPrize
+    ) BaseHook(_poolManager) VRFV2PlusWrapperConsumerBase(_vrfV2PlusWrapper) {
         // Set initial pool fee in bps
         initialPoolFee = _initialPoolFee;
         // Calulate fee percentage in bps to be taken from fee
@@ -82,11 +127,11 @@ contract LPYieldSpike is BaseHook {
             beforeInitialize: true,
             afterInitialize: true,
             beforeAddLiquidity: false,
-            afterAddLiquidity: false,
+            afterAddLiquidity: true,
             beforeRemoveLiquidity: false,
-            afterRemoveLiquidity: false,
+            afterRemoveLiquidity: true,
             beforeSwap: true,
-            afterSwap: false,
+            afterSwap: true,
             beforeDonate: false,
             afterDonate: false,
             beforeSwapReturnDelta: false,
@@ -107,7 +152,99 @@ contract LPYieldSpike is BaseHook {
         return this.afterInitialize.selector;
     }
 
-    function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
+    function afterAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4, BalanceDelta) {
+        PoolId poolId = key.toId();
+
+        // Calculate the square root prices for the current position
+        uint160 sqrtPriceAX96 = TickMath.getSqrtPriceAtTick(params.tickLower);
+        uint160 sqrtPriceBX96 = TickMath.getSqrtPriceAtTick(params.tickUpper);
+        uint256 token0Amount = LiquidityAmounts.getAmount0ForLiquidity(
+            sqrtPriceAX96, sqrtPriceBX96, uint128(uint256(params.liquidityDelta))
+        );
+
+        lpPositionsInfo[poolId][sender].push(
+            LPPositionInfo({
+                tickLower: params.tickLower,
+                tickUpper: params.tickUpper,
+                liquidityDelta: params.liquidityDelta,
+                token0Amount: token0Amount
+            })
+        );
+        lpProviders[poolId].push(sender);
+        numberOfPositions[poolId]++;
+
+        return (this.afterAddLiquidity.selector, BalanceDeltaLibrary.ZERO_DELTA);
+    }
+
+    function afterRemoveLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4, BalanceDelta) {
+        PoolId poolId = key.toId();
+
+        // Get current positions for this sender in this pool
+        LPPositionInfo[] storage currentPositions = lpPositionsInfo[poolId][sender];
+
+        // Find and update the matching position
+        for (uint256 i = 0; i < currentPositions.length; i++) {
+            // Check if this position matches the removed liquidity's tick range
+            if (currentPositions[i].tickLower == params.tickLower && currentPositions[i].tickUpper == params.tickUpper)
+            {
+                // Update the liquidity delta
+                // params.liquidityDelta will be negative when removing liquidity
+                currentPositions[i].liquidityDelta += params.liquidityDelta;
+
+                // If liquidity becomes zero, consider removing the position entirely
+                if (currentPositions[i].liquidityDelta == 0) {
+                    // Remove this position by replacing it with the last position and then pop
+                    currentPositions[i] = currentPositions[currentPositions.length - 1];
+                    currentPositions.pop();
+
+                    numberOfPositions[poolId]--;
+
+                    // Check if this sender has no more positions in this pool
+                    if (currentPositions.length == 0) {
+                        // Remove the LP provider from the lpProviders array for this pool
+                        address[] storage providers = lpProviders[poolId];
+                        for (uint256 j = 0; j < providers.length; j++) {
+                            if (providers[j] == sender) {
+                                // Replace with last element and pop
+                                providers[j] = providers[providers.length - 1];
+                                providers.pop();
+                                break;
+                            }
+                        }
+                    }
+
+                    break;
+                } else {
+                    // Recalculate token0 amount for the updated position
+                    uint160 sqrtPriceAX96 = TickMath.getSqrtPriceAtTick(params.tickLower);
+                    uint160 sqrtPriceBX96 = TickMath.getSqrtPriceAtTick(params.tickUpper);
+                    currentPositions[i].token0Amount = LiquidityAmounts.getAmount0ForLiquidity(
+                        sqrtPriceAX96, sqrtPriceBX96, uint128(uint256(currentPositions[i].liquidityDelta))
+                    );
+                }
+
+                break;
+            }
+        }
+
+        return (this.afterRemoveLiquidity.selector, BalanceDeltaLibrary.ZERO_DELTA);
+    }
+
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
         external
         override
         onlyPoolManager
@@ -119,6 +256,26 @@ contract LPYieldSpike is BaseHook {
         accumulatePrize(key, params);
         // Return the hook selector, zero delta (no modification to swap), and zero additional data
         return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, updatedPoolFee);
+    }
+
+    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+        external
+        override
+        returns (bytes4, int128)
+    {
+        if (block.timestamp > lastPrizeDrawTimestamp[key.toId()] + PRIZE_DRAW_INTERVAL) {
+            _requestRandomWord(key);
+        }
+
+        return (this.afterSwap.selector, 0);
+    }
+
+    function deposit(uint256 amount) external {
+        unchecked {
+            linkTokenBalance += amount;
+        }
+        i_linkToken.transferFrom(msg.sender, address(this), amount);
+        emit Deposit(msg.sender, amount);
     }
 
     function handlePoolFeeUpdate(PoolKey calldata key) internal returns (uint24) {
@@ -148,5 +305,79 @@ contract LPYieldSpike is BaseHook {
         Currency feeCurrency = params.zeroForOne ? key.currency0 : key.currency1;
         // Transfer the accumulated prize amount from the swapper to the hook contract
         poolManager.take(feeCurrency, address(this), amountToAccumulateToPrize);
+
+        accumulatedPrize[key.toId()][feeCurrency] += amountToAccumulateToPrize;
+    }
+
+    /**
+     * @dev Fulfills the VRF request identified by the given request ID with the randomWords provided.
+     */
+    // solhint-disable-next-line
+    function fulfillRandomWords(uint256 chainlinkVRFRequestID, uint256[] memory randomWords) internal override {
+        PoolKey memory key = requests[chainlinkVRFRequestID];
+        if (key.tickSpacing == 0) revert InvalidChainlinkVRFRequestID();
+
+        PoolId poolId = key.toId();
+
+        uint256 totalNumberOfPositionsInPool = numberOfPositions[poolId];
+
+        uint256 totalEntriesCount;
+        uint256[] memory currentEntryIndexArray = new uint256[](totalNumberOfPositionsInPool);
+
+        address[] memory poolLPs = lpProviders[poolId];
+
+        for (uint256 i = 0; i < poolLPs.length; i++) {
+            // Get current positions for this sender in this pool
+            LPPositionInfo[] storage currentPositions = lpPositionsInfo[poolId][poolLPs[i]];
+            for (uint256 j = 0; j < currentPositions.length; j++) {
+                uint256 totalToken0Amount = currentPositions[i].token0Amount;
+                uint256 entriesCount = totalToken0Amount / VALUE_PER_ENTRY;
+                totalEntriesCount += entriesCount;
+
+                // Calculate current entry index for this LP provider
+                currentEntryIndexArray[j] = j == 0 ? entriesCount - 1 : currentEntryIndexArray[j - 1] + entriesCount;
+
+                entryIndexToProvider[currentEntryIndexArray[j]] = poolLPs[i];
+            }
+        }
+
+        // Determine the winner based on the random number
+        uint256 randomWord = randomWords[0];
+        uint256 winningEntry = randomWord % totalEntriesCount;
+
+        address winner = entryIndexToProvider[winningEntry];
+
+        if (accumulatedPrize[poolId][key.currency0] > 0) {
+            key.currency0.transfer(winner, accumulatedPrize[poolId][key.currency0]);
+            accumulatedPrize[poolId][key.currency0] = 0;
+        }
+
+        if (accumulatedPrize[poolId][key.currency1] > 0) {
+            key.currency1.transfer(winner, accumulatedPrize[poolId][key.currency1]);
+            accumulatedPrize[poolId][key.currency1] = 0;
+        }
+    }
+
+    /**
+     * @dev Called when a time comes to draw a new prize winner
+     */
+    function _requestRandomWord(PoolKey calldata key) internal {
+        uint256 requestPrice = i_vrfV2PlusWrapper.calculateRequestPrice(VRF_CALLBACK_GAS_LIMIT, 1);
+
+        // Ensure that hook has enough link token to pay VRF request
+        if (requestPrice > linkTokenBalance) revert InsufficientBalance();
+        unchecked {
+            linkTokenBalance -= requestPrice;
+        }
+
+        // Pass the request on the Chainlink VRF coordinator
+        (uint256 chainlinkVRFRequestID,) = requestRandomness(
+            VRF_CALLBACK_GAS_LIMIT,
+            minimumConfirmations,
+            1,
+            VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+        );
+
+        requests[chainlinkVRFRequestID] = key;
     }
 }
--- test/LPYieldSpike.t.sol
+++ test/LPYieldSpike.t.sol
@@ -7,11 +7,14 @@ import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
 import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
 
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {PoolManager} from "v4-core/PoolManager.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 
 import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
 
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {TickMath} from "v4-core/libraries/TickMath.sol";
@@ -22,12 +25,17 @@ import {console} from "forge-std/console.sol";
 import {LPYieldSpike} from "../src/LPYieldSpike.sol";
 
 contract TestLPYieldHook is Test, Deployers {
+    using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
 
     MockERC20 token;
 
     Currency ethCurrency = Currency.wrap(address(0));
     Currency tokenCurrency;
+
+    uint24 initialPoolFee = 500;
+    uint24 feePercentageForPrize = 500;
 
     LPYieldSpike hook;
 
@@ -35,9 +43,6 @@ contract TestLPYieldHook is Test, Deployers {
         deployFreshManagerAndRouters();
 
         deployMintAndApprove2Currencies();
-
-        uint24 initialPoolFee = 500;
-        uint24 feePercentageForPrize = 500;
 
         // Deploy our hook
         address hookAddress =
@@ -54,14 +59,41 @@ contract TestLPYieldHook is Test, Deployers {
         // Add liquidity to the pool
         modifyLiquidityRouter.modifyLiquidity(
             key,
-            IPoolManager.ModifyLiquidityParams({tickLower: -60, tickUpper: 60, liquidityDelta: 100e18, salt: bytes32(0)}),
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: -60,
+                tickUpper: 60,
+                liquidityDelta: 100 ether,
+                salt: bytes32(0)
+            }),
             ZERO_BYTES
         );
     }
 
-    function test_initialFee() public {
-        (,,, uint24 fee) = manager.getSlot0(poolId);
-        assertEq(fee, 3000);
+    function test_initialFee() public view {
+        (,,, uint24 fee) = manager.getSlot0(key.toId());
+        assertEq(fee, initialPoolFee);
+    }
+
+    // Swap with an updated fee and verify the output
+    function test_updatedFeeBeforeSwap() public {
+        // uint24 newPoolFee = 300;
+        // // update the LP fee, and verify the fee is updated
+        // hook.forceUpdateLPFee(key, newPoolFee);
+        // (,,, uint24 fee) = manager.getSlot0(key.toId());
+        // assertEq(fee, newPoolFee);
+
+        // uint256 currency1Before = currency1.balanceOfSelf();
+
+        // // Perform a test swap //
+        // bool zeroForOne = true;
+        // int256 amountSpecified = -1e18; // negative number indicates exact input swap!
+        // BalanceDelta swapDelta = swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+        // // ------------------- //
+
+        // uint256 currency1After = currency1.balanceOfSelf();
+
+        // // the fee is 0.20% so we should receive approximately 0.9980 of currency1
+        // assertApproxEqAbs(currency1After - currency1Before, 0.998e18, 0.0001 ether);
     }
 
     function test_a() public {
@@ -78,132 +110,8 @@ contract TestLPYieldHook is Test, Deployers {
         uint256 balanceOfToken1Before = currency1.balanceOfSelf();
         swapRouter.swap(key, params, testSettings, ZERO_BYTES);
         uint256 balanceOfToken1After = currency1.balanceOfSelf();
-        uint256 outputFromBaseFeeSwap = balanceOfToken1After - balanceOfToken1Before;
+        // uint256 outputFromBaseFeeSwap = balanceOfToken1After - balanceOfToken1Before;
 
         assertGt(balanceOfToken1After, balanceOfToken1Before);
     }
-
-    // function test_addLiquidityAndSwap() public {
-    //     // Set no referrer in the hook data
-    //     bytes memory hookData = abi.encode(address(0), address(this));
-
-    //     uint256 pointsBalanceOriginal = hook.balanceOf(address(this));
-
-    //     // amount0Delta = ~0.003 ETH
-    //     // How we landed on 0.003 ether here is based on computing value of x and y given
-    //     // total value of delta L (liquidity delta) = 1 ether
-    //     // This is done by computing x and y from the equation shown in Ticks and Q64.96 Numbers lesson
-    //     // View the full code for this lesson on GitHub which has additional comments
-    //     // showing the exact computation and a Python script to do that calculation for you
-
-    //     uint160 sqrtPriceAtTickLower = TickMath.getSqrtPriceAtTick(-60);
-    //     uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
-
-    //     (uint256 amount0Delta, uint256 amount1Delta) =
-    //         LiquidityAmounts.getAmountsForLiquidity(SQRT_PRICE_1_1, sqrtPriceAtTickLower, sqrtPriceAtTickUpper, 1 ether);
-
-    //     modifyLiquidityRouter.modifyLiquidity{value: amount0Delta + 1}(
-    //         key,
-    //         IPoolManager.ModifyLiquidityParams({
-    //             tickLower: -60,
-    //             tickUpper: 60,
-    //             liquidityDelta: 1 ether,
-    //             salt: bytes32(0)
-    //         }),
-    //         hookData
-    //     );
-
-    //     uint256 pointsBalanceAfterAddLiquidity = hook.balanceOf(address(this));
-
-    //     // The exact amount of ETH we're adding (x)
-    //     // is roughly 0.299535... ETH
-    //     // Our original POINTS balance was 0
-    //     // so after adding liquidity we should have roughly 0.299535... POINTS tokens
-    //     assertApproxEqAbs(
-    //         pointsBalanceAfterAddLiquidity - pointsBalanceOriginal,
-    //         2995354955910434,
-    //         0.0001 ether // error margin for precision loss
-    //     );
-
-    //     // Now we swap
-    //     // We will swap 0.001 ether for tokens
-    //     // We should get 20% of 0.001 * 10**18 points
-    //     // = 2 * 10**14
-    //     swapRouter.swap{value: 0.001 ether}(
-    //         key,
-    //         IPoolManager.SwapParams({
-    //             zeroForOne: true,
-    //             amountSpecified: -0.001 ether,
-    //             sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
-    //         }),
-    //         PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}),
-    //         hookData
-    //     );
-
-    //     uint256 pointsBalanceAfterSwap = hook.balanceOf(address(this));
-
-    //     assertEq(pointsBalanceAfterSwap - pointsBalanceAfterAddLiquidity, 2 * 10 ** 14);
-    // }
-
-    // function test_addLiquidityAndSwapWithReferral() public {
-    //     bytes memory hookData = abi.encode(address(1), address(this));
-
-    //     uint256 pointsBalanceOriginal = hook.balanceOf(address(this));
-    //     uint256 referrerPointsBalanceOriginal = hook.balanceOf(address(1));
-
-    //     // amount0Delta = ~0.003 ETH
-    //     // How we landed on 0.003 ether here is based on computing value of x and y given
-    //     // total value of delta L (liquidity delta) = 1 ether
-    //     // This is done by computing x and y from the equation shown in Ticks and Q64.96 Numbers lesson
-    //     // View the full code for this lesson on GitHub which has additional comments
-    //     // showing the exact computation and a Python script to do that calculation for you
-
-    //     uint160 sqrtPriceAtTickLower = TickMath.getSqrtPriceAtTick(-60);
-    //     uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
-
-    //     (uint256 amount0Delta, uint256 amount1Delta) =
-    //         LiquidityAmounts.getAmountsForLiquidity(SQRT_PRICE_1_1, sqrtPriceAtTickLower, sqrtPriceAtTickUpper, 1 ether);
-
-    //     modifyLiquidityRouter.modifyLiquidity{value: amount0Delta + 1}(
-    //         key,
-    //         IPoolManager.ModifyLiquidityParams({
-    //             tickLower: -60,
-    //             tickUpper: 60,
-    //             liquidityDelta: 1 ether,
-    //             salt: bytes32(0)
-    //         }),
-    //         hookData
-    //     );
-
-    //     uint256 pointsBalanceAfterAddLiquidity = hook.balanceOf(address(this));
-    //     uint256 referrerPointsBalanceAfterAddLiquidity = hook.balanceOf(address(1));
-
-    //     assertApproxEqAbs(pointsBalanceAfterAddLiquidity - pointsBalanceOriginal, 2995354955910434, 0.00001 ether);
-    //     assertApproxEqAbs(
-    //         referrerPointsBalanceAfterAddLiquidity - referrerPointsBalanceOriginal - hook.POINTS_FOR_REFERRAL(),
-    //         299535495591043,
-    //         0.000001 ether
-    //     );
-
-    //     // Now we swap
-    //     // We will swap 0.001 ether for tokens
-    //     // We should get 20% of 0.001 * 10**18 points
-    //     // = 2 * 10**14
-    //     // Referrer should get 10% of that - so 2 * 10**13
-    //     swapRouter.swap{value: 0.001 ether}(
-    //         key,
-    //         IPoolManager.SwapParams({
-    //             zeroForOne: true,
-    //             amountSpecified: -0.001 ether,
-    //             sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
-    //         }),
-    //         PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}),
-    //         hookData
-    //     );
-    //     uint256 pointsBalanceAfterSwap = hook.balanceOf(address(this));
-    //     uint256 referrerPointsBalanceAfterSwap = hook.balanceOf(address(1));
-
-    //     assertEq(pointsBalanceAfterSwap - pointsBalanceAfterAddLiquidity, 2 * 10 ** 14);
-    //     assertEq(referrerPointsBalanceAfterSwap - referrerPointsBalanceAfterAddLiquidity, 2 * 10 ** 13);
-    // }
 }
